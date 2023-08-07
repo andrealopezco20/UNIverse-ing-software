4716f8b96b2bb753597537d4396e7b5630c480ee
andrealopezco20 <104209000+andrealopezco20@users.noreply.github.com>
2023-08-06 19:52:13 -0500

    Update Home.jsx

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,10 +3,11 @@ import PostSide from '../../components/PostSide/PostSide'
 import ProfileSide from '../../components/profileSide/ProfileSide'
 import RightSide from '../../components/RightSide/RightSide'
 import './Home.css'
+//llamamos a las funciones para formar toda la pantalla de inicio
 const Home = () => {
   return (
     <div className="Home">
-        <ProfileSide/>
+        <ProfileSide/> 
         <PostSide/>
         <RightSide/>
     </div>
